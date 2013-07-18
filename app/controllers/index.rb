f0c162841288be8eaec3eacdb95e882867209c2b
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-18 16:10:36 -0500

    finished edit blog

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -8,40 +8,6 @@ end
 
 # ----- POSTS -----
 
-get '/new' do
-  # @post = Post.create(params[:id])
-  # @message = "this is our individual post page"
-  erb :new
-end
 
-get '/post/:id' do
-  @post = Post.find(params[:id])
-  @message = "this is our individual post page"
-  erb :show
-end
-
-get '/post/:id/edit' do
-  # @post = Post.find(params[:id])
-  @message = "this is our individual edit page"
-  erb :edit
-end
-
-post '/' do
-   @post = Post.create(params[:post])
-  @message = "this is our individual new post page"
-  erb :show
-end
-
-post '/post/:id/edit' do
-  @post = Post.find(params[:id])
-  @post.update_attributes(params[:form])
-  @message = "this is our individual edit page"
-  erb :edit
-end
-
-post '/post/:id/delete' do
-  # @post = Post.destroy(params[:id])
-  redirect to '/'
-end
 
 # ----- TAGS -----
---
+++ app/controllers/posts.rb
@@ -0,0 +1,37 @@
+get '/posts/new' do
+  # @post = Post.create(params[:id])
+  # @message = "this is our individual post page"
+  erb :new
+end
+
+get '/posts/:id' do
+  @post = Post.find(params[:id])
+  @message = "this is our individual post page"
+  erb :show
+end
+
+get '/posts/:id/edit' do
+  puts params.inspect
+  @post = Post.find(params[:id])
+  @message = "this is our individual edit page"
+  erb :edit
+end
+
+post '/posts' do
+  puts params.inspect
+  @post = Post.create(params[:post])
+  @message = "this is our individual new post page"
+  erb :show
+end
+
+post '/posts/:id' do
+  @post = Post.find(params[:id])
+  @post.update_attributes(params[:post])
+  @message = "this is our individual edit page"
+  redirect to("/posts/#{@post.id}")
+end
+
+post '/posts/:id/delete' do
+  # @post = Post.destroy(params[:id])
+  redirect to '/'
+end
